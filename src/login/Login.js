29a6a511a226738cc02fd344c66a71289b5eafd2
shankerdhand <shankerdhand@gmail.com>
2020-07-05 08:59:13 +0530

    login class created

---
+++ src/login/Login.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class Login extends React.Component{
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>Dhruvit Dhand</h1>
+            </div>
+        )
+    }
+
+}
+
+export default Login;
